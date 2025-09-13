fbf3d673e9393ea76d041f01abb956d32c197cda
Sridhar Ramasamy <Sridhar.Ramasamy@cdw.com>
2025-09-13 23:40:39 +0530

    try again

--- s3module2.tf
+++ s3module2.tf
@@ -1,6 +1,6 @@
 module "mys3call2" {
   source = "./module/s3resource"
 
-  bucket_name = var.mybuckname
-  s3_tag      = var.mytagname
+  bucket_name = var.mybuckname2
+  s3_tag      = var.mytagname2
 }
--- variable.tf
+++ variable.tf
@@ -7,3 +7,13 @@ variable "mytagname" {
   type        = map(string)
   description = "Enter the tag name"
 }
+
+variable "mybuckname2" {
+  type        = string
+  description = "Enter the bucket name"
+}
+
+variable "mytagname2" {
+  type        = map(string)
+  description = "Enter the tag name"
+}
